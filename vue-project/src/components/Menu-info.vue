f2d0254397a5ece333edc3db5616e9fe2d849734
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2023-07-08 22:17:48 -0500

    FRONTED 3

--- vue-project/src/components/Menu-info.vue
+++ vue-project/src/components/Menu-info.vue
@@ -1,7 +1,7 @@
 
 <template>
     <div class="menu-info">
-        <h1>Menu info</h1>        
+        <h1>Hola {{ username }}</h1>        
     </div>
     
     
@@ -12,6 +12,27 @@
 <script>
 export default {
   props: ['name'], // Prop para recibir el valor de name
+
+  data() {
+    return {
+        username : this.getUsername()
+    }
+  },
+
+  methods: {
+      getUsername() {
+            // Obtiene la URL actual
+            var url = window.location.href;
+            console.log(url);
+
+            // Obtiene el nombre de la URL
+            var nameParam = new URL(url).searchParams.get("username");
+            var username = nameParam ? nameParam.split('=')[1] : '';
+            console.log(username);
+
+            return username;
+        },
+    }
 };
 </script>
 
